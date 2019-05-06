9c5dcdfcc7a2e548700f124f4bbfcd5c253d2c39
ValentinZeller <v.zeller@ludus-academie.com>
2019-05-06 23:53:45 +0200

    Ajout des opérations sur les vecteurs

--- main.pas
+++ main.pas
@@ -3,7 +3,7 @@ program main;
 uses unitGameObject,unitVect2D,unitBall,crt;
 
 var obj : gameObject;
-  vec : Vect2D;
+  vec,vec1,vec2 : Vect2D;
   balle : ball;
 begin
   clrscr;
@@ -33,5 +33,22 @@ begin
   writeln('Position X : ',getVectPosX(getObjPosition(getBallObj(balle))));
   writeln('Position Y : ',getVectPosY(getObjPosition(getBallObj(balle))));
   writeln('Texture : ',getObjTexture(getBallObj(balle)));
+  writeln;
+
+  //Test des opérations sur les vecteurs
+  setVectPosX(vec1,1);
+  setVectPosY(vec1,2);
+  setVectPosX(vec2,3);
+  setVectPosY(vec2,4);
+  writeln('Vecteur 1 : X : ',getVectPosX(vec1),' Y : ',getVectPosY(vec1));
+  writeln('Vecteur 2 : X : ',getVectPosX(vec2),' Y : ',getVectPosY(vec2));
+  writeln('Addition : Position X : ',getVectPosX(addVect(vec1,vec2)));
+  writeln('Addition : Position Y : ',getVectPosY(addVect(vec1,vec2)));
+  writeln('Soustraction : Position X : ',getVectPosX(sousVect(vec1,vec2)));
+  writeln('Soustraction : Position Y : ',getVectPosY(sousVect(vec1,vec2)));
+  writeln('Scalaire : Position X : ',getVectPosX(scalVect(vec1,2)));
+  writeln('Scalaire : Position Y : ',getVectPosY(scalVect(vec1,2)));
+  writeln('Produit scalaire entre 2 vecteurs : ',prodScalVect(vec1,vec2));
+  writeln('Produit vectoriel : ',prodVect(vec1,vec2));
   readln;
 end.
--- unitVect2D.pas
+++ unitVect2D.pas
@@ -11,6 +11,11 @@ function getVectPosX(position : Vect2D):integer;
 function getVectPosY(position : Vect2D):integer;
 procedure setVectPosX(var position : Vect2D;X : integer);
 procedure setVectPosY(var position : Vect2D;Y : integer);
+function addVect(vect1,vect2 : Vect2D):Vect2D;
+function sousVect(vect1,vect2 : Vect2D):Vect2D;
+function scalVect(vect1 : Vect2D;scal : integer):Vect2D;
+function prodScalVect(vect1,vect2 : Vect2D):integer;
+function prodVect(vect1,vect2 : Vect2D):integer;
 
 implementation
 
@@ -32,6 +37,55 @@ end;
 procedure setVectPosY(var position : Vect2D;Y : integer);
 begin
   position.posY := Y;
+end;
+
+function addVect(vect1,vect2 : Vect2D):Vect2D;
+//BUT : Addition de vecteurs
+//ENTREE : 2 vecteurs 2D
+//SORTIE : 1 vecteur 2D
+var vectemp : Vect2D;
+begin
+  vectemp.posX := vect1.posX + vect2.posX;
+  vectemp.posY := vect1.posY + vect2.posY;
+  addVect := vectemp;
+end;
+
+function sousVect(vect1,vect2 : Vect2D):Vect2D;
+//BUT : Soustraction de vecteurs
+//ENTREE : 2 vecteurs 2D
+//SORTIE : 1 vecteur 2D
+var vectemp : Vect2D;
+begin
+  vectemp.posX := vect1.posX - vect2.posX;
+  vectemp.posY := vect1.posY - vect2.posY;
+  sousVect := vectemp;
+end;
+
+function scalVect(vect1 : Vect2D;scal : integer):Vect2D;
+//BUT : Produit scalaire entre un vecteur et un nombre
+//ENTREE : 1 vecteur 2D et un entier
+//SORTIE : 1 vecteur 2D
+var vectemp : Vect2D;
+begin
+  vectemp.posX := vect1.posX * scal;
+  vectemp.posY := vect1.posY * scal;
+  scalVect := vectemp;
+end;
+
+function prodScalVect(vect1,vect2 : Vect2D):integer;
+//BUT : Produit scalaire entre deux vecteurs
+//ENTREE : 2 vecteurs 2D
+//SORTIE : 1 vecteur 2D
+begin
+  prodScalVect := vect1.posX*vect2.posX+vect1.posY*vect2.posY;
+end;
+
+function prodVect(vect1,vect2 : Vect2D): integer;
+//BUT : Produit vectoriel de deux vecteurs donnant le déterminant
+//ENTREE : 2 vecteurs 2D
+//SORTIE : 1 nombre
+begin
+  prodVect := vect1.posX * vect2.posY + vect1.posY * vect2.posX;
 end;
 
 end.
